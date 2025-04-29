02838c896c4e3e6138e15d0288b9158848a3a9fa
Mohammad Ali Ahmed <AAhmed0080@mindbridge.co>
2025-04-29 12:04:12 +0500

    a

---
+++ prisma/migrations/20250429055659_a/migration.sql
@@ -0,0 +1,29 @@
+-- DropIndex
+DROP INDEX `Cart_customerId_fkey` ON `cart`;
+
+-- DropIndex
+DROP INDEX `Images_productId_fkey` ON `images`;
+
+-- DropIndex
+DROP INDEX `Order_customerId_fkey` ON `order`;
+
+-- DropIndex
+DROP INDEX `OrderItem_orderId_fkey` ON `orderitem`;
+
+-- DropIndex
+DROP INDEX `OrderItem_productId_fkey` ON `orderitem`;
+
+-- AddForeignKey
+ALTER TABLE `Order` ADD CONSTRAINT `Order_customerId_fkey` FOREIGN KEY (`customerId`) REFERENCES `Customer`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Images` ADD CONSTRAINT `Images_productId_fkey` FOREIGN KEY (`productId`) REFERENCES `Product`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `OrderItem` ADD CONSTRAINT `OrderItem_orderId_fkey` FOREIGN KEY (`orderId`) REFERENCES `Order`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `OrderItem` ADD CONSTRAINT `OrderItem_productId_fkey` FOREIGN KEY (`productId`) REFERENCES `Product`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Cart` ADD CONSTRAINT `Cart_customerId_fkey` FOREIGN KEY (`customerId`) REFERENCES `Customer`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // ✅ Enable CORS for Angular dev server
+  app.enableCors({
+    origin: 'http://localhost:4200', // or use '*' during development
+    credentials: true
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
